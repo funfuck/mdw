16360a893c6d9f756b82385e40b82432807a6bec
narupon <narupon.p@infinitaskt.com>
2024-05-26 12:25:30 +0700

    call pii lib

--- mdw.go
+++ mdw.go
@@ -6,16 +6,12 @@ import (
 	"github.com/funfuck/pii"
 )
 
-func NewMDW(f func(interface{}) (string, error)) func() error {
+func NewMDW() func() error {
 	return func() error {
 
-		str, err := f(nil)
+		str, err := pii.Any(nil)
 		fmt.Printf("str: %v\n", str)
 		fmt.Printf("err: %v\n", err)
-
-		s, e := pii.Any(nil)
-		fmt.Printf("s: %v\n", s)
-		fmt.Printf("e: %v\n", e)
 
 		return err
 	}
