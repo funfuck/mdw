f54d5ce38f8aa2c613e615941d2ed0993825718d
narupon <narupon.p@infinitaskt.com>
2024-05-26 12:23:21 +0700

    call pii lib

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/funfuck/mdw
 
 go 1.22.3
+
+require github.com/funfuck/pii v0.0.0-20240526050450-ff2ee636e554
--- mdw.go
+++ mdw.go
@@ -1,6 +1,10 @@
 package mdw
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/funfuck/pii"
+)
 
 func NewMDW(f func(interface{}) (string, error)) func() error {
 	return func() error {
@@ -8,6 +12,10 @@ func NewMDW(f func(interface{}) (string, error)) func() error {
 		str, err := f(nil)
 		fmt.Printf("str: %v\n", str)
 		fmt.Printf("err: %v\n", err)
+
+		s, e := pii.Any(nil)
+		fmt.Printf("s: %v\n", s)
+		fmt.Printf("e: %v\n", e)
 
 		return err
 	}
